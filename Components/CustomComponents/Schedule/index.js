2e1b48f6994b3ed99ab3b5b0c7e4a84b72edb7aa
SaguaSantiago <saguasantiago@gmail.com>
2022-08-11 17:02:06 -0300

    styles eliminated and styled component added

--- Components/CustomComponents/Schedule/index.js
+++ Components/CustomComponents/Schedule/index.js
@@ -1,41 +1,81 @@
-import { Avatar, AvatarGroup, Button, Grid, IconButton, Typography } from '@mui/material'
+import { Avatar, AvatarGroup, Button, Grid, IconButton, styled, Typography } from '@mui/material'
 import React from 'react'
-import styles from './Schedule.module.css'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
 import WatchLaterOutlinedIcon from '@mui/icons-material/WatchLaterOutlined'
 
+const Container = styled(Grid)`
+  width: 90%;
+  margin: 50px auto;
+  min-height: 100px;
+  padding: 3%;
+`
+
+const Title = styled(Typography)`
+  margin-top: 2%;
+  font-size: 24px;
+  font-weight: bold;
+  color: var(--black-color);
+`
+
+const Subtitle = styled('h4')`
+  color: var(--grey-color-1);
+  font-family: Raleway, Arial;
+  font-weight: bold;
+  margin-left: 10px;
+`
+
+const StyledIconButton = styled(IconButton)`
+  background-color: var(--grey-color-3);
+  border-radius: 13px;
+  font-family: Raleway, Arial;
+  font-weight: bold;
+  margin-left: 10px;
+  position: relative;
+  right: 0;
+`
+
+const SubtitleContainer = styled('div')`
+  display: flex;
+  align-items: center;
+`
+
+const ButtonContainer = styled(Grid)`
+  display: flex;
+  align-items: start;
+  justify-content: end;
+`
+
 export default function Schedule({ title, avatars }) {
   return (
-    <Grid container justifyContent='space-between' className={styles.container}>
+    <Container container justifyContent='space-between'>
       <Grid xs={12} item>
         <Grid container justifyContent='space-between' wrap='wrap-reverse'>
           <Grid item xs={12} sm={6} md={8}>
             <div>
-              <Typography variant='h2' className={styles.title_txt}>
-                {title}
-              </Typography>
-              <div className={styles.subtitle}>
-                <WatchLaterOutlinedIcon className={styles.icon} />
-                <h4 className={styles.desc_txt}>desc</h4>
-              </div>
+              <Title variant='h2'>{title}</Title>
+              <SubtitleContainer>
+                <WatchLaterOutlinedIcon
+                  sx={{
+                    color: 'var(--grey-color-1)',
+                  }}
+                />
+                <Subtitle>desc</Subtitle>
+              </SubtitleContainer>
             </div>
           </Grid>
-          <Grid item xs={12} sm={2} md={2} className={styles.button_container}>
+
+          <ButtonContainer item xs={12} sm={2} md={2}>
             <div>
-              <IconButton className={styles.iconButton}>
+              <StyledIconButton>
                 <MoreHorizIcon />
-              </IconButton>
+              </StyledIconButton>
             </div>
-          </Grid>
+          </ButtonContainer>
         </Grid>
       </Grid>
 
       <Grid item>
-        <AvatarGroup
-          className={styles.avatarGroup_container}
-          max={8}
-          total={avatars ? avatars.length : null}
-        >
+        <AvatarGroup max={8} total={avatars ? avatars.length : null}>
           {avatars
             ? avatars.map((src) => (
                 <Avatar key={src} src={src}>
@@ -49,6 +89,6 @@ export default function Schedule({ title, avatars }) {
       <Grid item xs={1.5}>
         <Button fullWidth>JOIN</Button>
       </Grid>
-    </Grid>
+    </Container>
   )
 }
